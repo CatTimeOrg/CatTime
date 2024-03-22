8c50187bca38aba6464093491ecf47e57cb55812
Daniel Häfele <haefele@c-entron.de>
2024-03-22 23:59:43 +0100

    Code cleanup reading claims on the server side

---
+++ src/CatTime.Backend/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace CatTime.Backend.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetEmployeeId(this ClaimsPrincipal claimsPrincipal) => int.Parse(claimsPrincipal.FindFirst("EmployeeId").Value);
+    public static int GetCompanyId(this ClaimsPrincipal claimsPrincipal) => int.Parse(claimsPrincipal.FindFirst("CompanyId").Value);
+}
