286f04141a0fbc4c4a311385f5b498084184886c
Ena Jukic <jukic@c-entron.de>
2024-03-23 00:40:08 +0100

    Register and Login pages

---
+++ src/CatTime.Frontend/Pages/Auth/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatTime.Frontend.Pages.Auth.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+        public string? Password { get; set; }
+    }
+}
