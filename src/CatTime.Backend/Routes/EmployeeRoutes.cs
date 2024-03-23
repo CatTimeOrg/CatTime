28e7721c2b22b9dfa2f9efbc9034cbce461e325d
woudinix <woudinix>
2024-03-23 11:55:01 +0100

    Created new endpoints for employees to load the list of company employees and to load a single employee by id.

--- src/CatTime.Backend/Routes/EmployeeRoutes.cs
+++ src/CatTime.Backend/Routes/EmployeeRoutes.cs
@@ -1,4 +1,10 @@
-﻿namespace CatTime.Backend.Routes;
+﻿using CatTime.Backend.Database;
+using CatTime.Backend.Extensions;
+using CatTime.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatTime.Backend.Routes;
 
 public static class EmployeeRoutes
 {
@@ -7,6 +13,27 @@ public static class EmployeeRoutes
         var group = builder.MapGroup("/employees");
         group.RequireAuthorization();
 
-        
+        group.MapGet("/", async (CatContext catContext, HttpContext httpContext) =>
+        {
+            var companyId = httpContext.User.GetCompanyId();
+
+            var employees = await catContext.Employees.Where(e => e.Company.Id == companyId).ToListAsync();
+
+            return employees.Select(e => e.ToDTO()).ToList();
+        });
+
+        group.MapGet("/{id}", async (CatContext catContext, HttpContext httpContext, int id) =>
+        {
+            var companyId = httpContext.User.GetCompanyId();
+
+            var employee = await catContext.Employees.FirstOrDefaultAsync(e => e.Id == id && e.Company.Id == companyId);
+
+            if(employee == null)
+            {
+                return Results.Problem("Der Mitarbeiter wurde nicht gefunden oder sie haben kein Recht die Daten abzurufen.", statusCode: StatusCodes.Status400BadRequest);
+            }
+
+            return Results.Ok(employee.ToDTO());
+        });
     }
 }
