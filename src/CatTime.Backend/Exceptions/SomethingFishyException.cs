efed6eaf01e2a47e8f869ede52cbc573ed579390
woudinix <woudinix>
2024-03-23 13:04:17 +0100

    Added a new fishy exception for errors that shouldn't occur.

---
+++ src/CatTime.Backend/Exceptions/SomethingFishyException.cs
@@ -0,0 +1,10 @@
+﻿namespace CatTime.Backend.Exceptions
+{
+    public class SomethingFishyException : Exception
+    {
+        public SomethingFishyException() 
+            : base("Meow??? Something fishy is going on!")
+        {
+        }
+    }
+}
