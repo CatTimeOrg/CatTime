a5c806addcc4e4008115e5526be81d50dd6f8f03
Ena Jukic <jukic@c-entron.de>
2024-03-23 21:57:02 +0100

    MyWorkTimes page UI and refactoring

---
+++ src/CatTime.Frontend/Pages/Content/Models/MyTimeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CatTime.Frontend.Pages.Content.Models;
+
+public class MyTimeModel
+{
+    public DateTime? Date { get; set; }
+    public DateTime? StartWorkTime { get; set; }
+    public DateTime? EndWorkTime { get; set; }
+    public string? TotalWorkingTime { get; set; }
+}
