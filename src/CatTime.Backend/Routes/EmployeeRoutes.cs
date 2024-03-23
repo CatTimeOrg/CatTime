6c4d067f700ed2ce5aaa3cd6aafb7c44bb8ff1a9
woudinix <woudinix>
2024-03-23 13:04:35 +0100

    Implemented the create employee route.

--- src/CatTime.Backend/Routes/EmployeeRoutes.cs
+++ src/CatTime.Backend/Routes/EmployeeRoutes.cs
@@ -1,8 +1,13 @@
 ﻿using CatTime.Backend.Database;
+using CatTime.Backend.Database.Entities;
+using CatTime.Backend.Exceptions;
 using CatTime.Backend.Extensions;
 using CatTime.Shared;
+using CatTime.Shared.Routes.Auth;
+using CatTime.Shared.Routes.Employees;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace CatTime.Backend.Routes;
 
@@ -15,25 +20,52 @@ public static class EmployeeRoutes
 
         group.MapGet("/", async (CatContext catContext, HttpContext httpContext) =>
         {
-            var companyId = httpContext.User.GetCompanyId();
-
-            var employees = await catContext.Employees.Where(e => e.Company.Id == companyId).ToListAsync();
+            var employees = await catContext.Employees.ToListAsync();
 
             return employees.Select(e => e.ToDTO()).ToList();
         });
 
+        group.MapPost("/", async (CreateEmployeeRequest createEmployeeRequest, CatContext catContext, HttpContext httpContext) =>
+        {
+            var currentEmployee = await catContext.Employees.FindAsync(httpContext.User.GetEmployeeId()) ?? throw new SomethingFishyException();
+            if (currentEmployee.Role != EmployeeRole.Admin)
+            {
+                return Results.Problem("Sie haben keine Berechtigung um Mitarbeiter hinzuzufügen.", statusCode: StatusCodes.Status403Forbidden);
+            }
+
+            var existingEmployee = await catContext.Employees.FirstOrDefaultAsync(e => e.EmailAddress == createEmployeeRequest.EmailAddress);
+            if (existingEmployee != null)
+            {
+                return Results.Problem("E-Mail-Adresse bereits vergeben.", statusCode: StatusCodes.Status400BadRequest);
+            }
+
+            var newEmployee = new Employee
+            {
+                CompanyId = currentEmployee.CompanyId,
+                FirstName = createEmployeeRequest.FirstName,
+                LastName = createEmployeeRequest.LastName,
+                EmailAddress = createEmployeeRequest.EmailAddress,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(createEmployeeRequest.Password),
+                Role = createEmployeeRequest.Role
+            };
+
+            await catContext.Employees.AddAsync(newEmployee);
+            await catContext.SaveChangesAsync();
+
+            return Results.Ok(newEmployee.ToDTO());
+        });
+
         group.MapGet("/{id}", async (CatContext catContext, HttpContext httpContext, int id) =>
         {
-            var companyId = httpContext.User.GetCompanyId();
-
-            var employee = await catContext.Employees.FirstOrDefaultAsync(e => e.Id == id && e.Company.Id == companyId);
-
+            var employee = await catContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
             if(employee == null)
             {
-                return Results.Problem("Der Mitarbeiter wurde nicht gefunden oder sie haben kein Recht die Daten abzurufen.", statusCode: StatusCodes.Status400BadRequest);
+                return Results.Problem($"Es wurde kein Mitarbeiter für Id:{id} gefunden.", statusCode: StatusCodes.Status400BadRequest);
             }
 
             return Results.Ok(employee.ToDTO());
         });
+
+        
     }
 }
---
+++ src/CatTime.Shared/Routes/Employees/CreateEmployeeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatTime.Shared.Routes.Employees
+{
+    public class CreateEmployeeRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public EmployeeRole Role { get; set; }
+    }
+}
