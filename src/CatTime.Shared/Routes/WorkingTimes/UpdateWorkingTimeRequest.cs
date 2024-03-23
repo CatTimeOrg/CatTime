bf367b45f87c2f053a8d890005e84453bdd717e5
Daniel Häfele <haefele@c-entron.de>
2024-03-23 13:59:59 +0100

    Add more /workingtime endpoints

---
+++ src/CatTime.Shared/Routes/WorkingTimes/UpdateWorkingTimeRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CatTime.Shared.Routes.WorkingTimes;
+
+public class UpdateWorkingTimeRequest
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly Start { get; set; }
+    public TimeOnly? End { get; set; }
+    
+    public WorkingTimeType Type { get; set; }
+}
