25f90b9e3cb0e2bb37dbd86b1adbaf64b8d724a4
Daniel Häfele <haefele@c-entron.de>
2024-03-23 13:28:28 +0100

    Implement GET /worktime endpoint

--- src/CatTime.Backend/Routes/WorkingTimeRoutes.cs
+++ src/CatTime.Backend/Routes/WorkingTimeRoutes.cs
@@ -3,6 +3,7 @@ using CatTime.Backend.Database.Entities;
 using CatTime.Backend.Extensions;
 using CatTime.Shared;
 using CatTime.Shared.Routes.WorkingTimes;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatTime.Backend.Routes;
 
@@ -43,6 +44,30 @@ public static class WorkingTimeRoutes
             await catContext.SaveChangesAsync();
             
             return Results.Ok(workingTimeEntity.ToDTO());
+        });
+
+        group.MapGet("/", async (int? employeeId, DateOnly? from, DateOnly? to, CatContext catContext, HttpContext httpContext) =>
+        {
+            var targetEmployee = await catContext.Employees.FindAsync(employeeId ?? httpContext.User.GetEmployeeId());
+            if (targetEmployee is null)
+            {
+                return Results.Problem("Mitarbeiter nicht gefunden.", statusCode: StatusCodes.Status400BadRequest);
+            }
+            
+            var currentEmployee = await catContext.Employees.FindAsync(httpContext.User.GetEmployeeId());
+            if (currentEmployee != targetEmployee && currentEmployee.Role is not EmployeeRole.Admin)
+            {
+                return Results.Problem("Nicht autorisiert Zeiten für einen anderen Mitarbeiter aufzulisten.", statusCode: StatusCodes.Status400BadRequest);
+            }
+            
+            var actualFrom = from ?? new DateOnly(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var actualTo = to ?? new DateOnly(DateTime.Today.Year, DateTime.Today.Month, DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month));
+            
+            var workingTimes = await catContext.WorkingTimes
+                .Where(w => w.EmployeeId == targetEmployee.Id && w.Date >= actualFrom && w.Date <= actualTo)
+                .ToListAsync();
+            
+            return Results.Ok(workingTimes.Select(w => w.ToDTO()));
         });
     }
 }
