a21f348ddc33f0d5b9f1c0f238cf345cba102ddd
Ena Jukic <jukic@c-entron.de>
2024-03-23 22:55:58 +0100

    Edit work time and refactoring

--- src/CatTime.Frontend/Pages/Content/Models/MyTimeModel.cs
+++ src/CatTime.Frontend/Pages/Content/Models/MyTimeModel.cs
@@ -1,9 +1,24 @@
-﻿namespace CatTime.Frontend.Pages.Content.Models;
+﻿using CatTime.Shared;
+
+namespace CatTime.Frontend.Pages.Content.Models;
 
 public class MyTimeModel
 {
     public DateTime? Date { get; set; }
-    public DateTime? StartWorkTime { get; set; }
-    public DateTime? EndWorkTime { get; set; }
-    public string? TotalWorkingTime { get; set; }
+    public TimeOnly? Start { get; set; }
+    public TimeOnly? End { get; set; }
+    public WorkingTimeType Type { get; set; }
+    public string TotalWorkingTime
+    {
+        get
+        {
+            if (this.Start.HasValue && this.End.HasValue)
+            {
+                var difference = this.End.Value - this.Start.Value;
+                return difference.ToString(@"hh\:mm");
+            }
+            
+            return "00:00";
+        }
+    }
 }
