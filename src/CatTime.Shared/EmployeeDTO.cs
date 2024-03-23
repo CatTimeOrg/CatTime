784e833886e2652e7a4d5f36c249d94963ce76f4
woudinix <woudinix>
2024-03-23 11:57:37 +0100

    Added firstname and lastname to employeeDTO.

--- src/CatTime.Shared/EmployeeDTO.cs
+++ src/CatTime.Shared/EmployeeDTO.cs
@@ -4,7 +4,12 @@ public class EmployeeDTO : BaseEntity
 {
     public int CompanyId { get; set; }
 
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
     public string EmailAddress { get; set; }
+
     public string PasswordHash { get; set; }
 
     public EmployeeRole Role { get; set; }
