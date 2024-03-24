f6f036f5df1acc41c1fd9425eb1e588fa80c7597
Daniel Häfele <haefele@c-entron.de>
2024-03-24 13:53:52 +0100

    Code cleanup and data validation in some endpoints

--- src/CatTime.Backend/Database/Entities/Company.cs
+++ src/CatTime.Backend/Database/Entities/Company.cs
@@ -1,8 +1,19 @@
-﻿namespace CatTime.Backend.Database.Entities;
+﻿using CatTime.Shared;
+
+namespace CatTime.Backend.Database.Entities;
 
 public class Company : BaseEntity
 {
     public string Name { get; set; }
     public List<Employee> Employees { get; set; }
+
+    public CompanyDTO ToDTO()
+    {
+        return new CompanyDTO
+        {
+            Id = this.Id,
+            Name = this.Name
+        };
+    }
 }
 
