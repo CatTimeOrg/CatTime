6d3f44fd90929ef53a529ee2f7b3b760b266a465
woudinix <woudinix>
2024-03-24 15:07:47 +0100

    db migration

---
+++ src/CatTime.Backend/Database/Migrations/20240324140005_AddedDepartmentPhoneNumberToEmployee.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatTime.Backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDepartmentPhoneNumberToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Employees");
+        }
+    }
+}
