633aa66404cd049ee2bc9eef2c18e88b7b0a8c74
Daniel Häfele <haefele@c-entron.de>
2024-03-24 14:51:03 +0100

    Add new /attendance endpoint to get a overview of the currently checked-in employees

---
+++ src/CatTime.Frontend/Infrastructure/Service/ClientService.Attendance.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Json;
+using CatTime.Shared.Routes.Attendances;
+
+namespace CatTime.Frontend.Infrastructure.Service;
+
+public partial class ClientService
+{
+    public async Task<List<AttendanceDTO>> GetAttendances()
+    {
+        var response = await this._httpClient.GetAsync("/attendance");
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<List<AttendanceDTO>>();
+    }
+}
