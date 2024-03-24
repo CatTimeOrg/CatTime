b4a0b8cd00e41030c1baec0c72992f135239e23c
woudinix <woudinix>
2024-03-24 13:42:35 +0100

    Renamed ClientService group files. And made httpClient private again.

--- src/CatTime.Frontend/Infrastructure/Service/ClientService.cs
+++ src/CatTime.Frontend/Infrastructure/Service/ClientService.cs
@@ -10,7 +10,7 @@ namespace CatTime.Frontend.Infrastructure.Service
 {
     public partial class ClientService
     {
-        protected readonly HttpClient _httpClient;
+        private readonly HttpClient _httpClient;
 
         public ClientService(HttpClient httpClient)
         {
@@ -22,52 +22,6 @@ namespace CatTime.Frontend.Infrastructure.Service
             this._httpClient.DefaultRequestHeaders.Authorization = string.IsNullOrWhiteSpace(accessToken) is false
                 ? new AuthenticationHeaderValue("Bearer", accessToken)
                 : null;
-        }
-
-        public async Task<LoginResponse> Login(string emailAddress, string password)
-        {
-            var request = new LoginRequest
-            {
-                EmailAddress = emailAddress,
-                Password = password,
-            };
-            var response = await this._httpClient.PostAsJsonAsync("/auth/login", request);
-            response.EnsureSuccessStatusCode();
-            
-            return await response.Content.ReadFromJsonAsync<LoginResponse>();
-        }
-
-        public async Task<LoginResponse> Refresh(string refreshToken)
-        {
-            var request = new RefreshRequest
-            {
-                RefreshToken = refreshToken
-            };
-            var response = await this._httpClient.PostAsJsonAsync("/auth/refresh", request);
-            response.EnsureSuccessStatusCode();
-            
-            return await response.Content.ReadFromJsonAsync<LoginResponse>();
-        }
-        
-        public async Task Register(string firstName, string lastName, string emailAddress, string password)
-        {
-            var request = new RegisterRequest
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                EmailAddress = emailAddress,
-                Password = password
-            };
-            var response = await this._httpClient.PostAsJsonAsync("/auth/register", request);
-            response.EnsureSuccessStatusCode();
-        }
-
-        public async Task<EmployeeDTO> Me()
-        {
-            var response = await this._httpClient.GetAsync("/auth/me");
-            response.EnsureSuccessStatusCode();
-            
-            return await response.Content.ReadFromJsonAsync<EmployeeDTO>();
         }
 
         public async Task<CompanyDTO> GetCompany()
