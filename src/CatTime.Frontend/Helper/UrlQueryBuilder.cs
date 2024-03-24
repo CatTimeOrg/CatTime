b6a9068024137468a2528a70328e88bfcb7518dc
woudinix <woudinix>
2024-03-24 13:07:57 +0100

    Created a helper class that adds query parameters to an url that is send to the web api and used it for ClientService methods.

---
+++ src/CatTime.Frontend/Helper/UrlQueryBuilder.cs
@@ -0,0 +1,68 @@
+﻿
+namespace CatTime.Frontend.Helper
+{
+    public class UrlQueryBuilder
+    {
+        private string _startUrl;
+        private List<KeyValuePair<string, object>> _parameters;
+
+        public UrlQueryBuilder(string startUrl)
+        {
+            this._startUrl = startUrl;
+            this._parameters = new List<KeyValuePair<string, object>>();
+        }
+
+        public void AddParameter(string parameterName, object? parameterValue, bool condition = true)
+        {
+            if (parameterName == null || parameterValue == null || condition == false)
+                return;
+
+            this._parameters.Add(new KeyValuePair<string, object>(parameterName, parameterValue));
+        }
+
+        public string ToQueryString()
+        {
+            var url = this._startUrl;
+
+            foreach (var parameter in this._parameters)
+            {
+                url = this.AddQueryParameterToUrl(url, parameter.Key, this.ParameterValueToString(parameter.Value));
+            }
+
+            return url;
+        }
+
+        private string ParameterValueToString(object value)
+        {
+            if(value is string stringValue)
+                return stringValue;
+
+            if (value is DateOnly dateOnly)
+                return (dateOnly.ToString("yyyy-MM-dd"));
+
+            if (value is TimeOnly timeOnly)
+                return (timeOnly.ToString("HH:mm:ss"));
+
+            if(value is DateTime dateTime)
+                return (dateTime.ToString("yyyy-MM-ddTHH:mm:ssz"));
+
+            if(value is int || value is long || value is double || value is float || value is decimal)
+                return value.ToString() ?? string.Empty;
+
+            if(value is bool boolValue)
+                return boolValue.ToString().ToLower();
+
+            return string.Empty;
+        }
+
+        private string AddQueryParameterToUrl(string url, string parameterName, string? parameterValue)
+        {
+            if (parameterValue == null)
+                return url;
+
+            var seperator = url.Contains("?") ? "&" : "?";
+
+            return $"{url}{seperator}{parameterName}={parameterValue}";
+        }
+    }
+}
