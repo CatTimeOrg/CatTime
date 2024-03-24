caedb3f914167a85b2eb4026e204cd2dbe897858
Daniel Häfele <haefele@c-entron.de>
2024-03-24 13:28:02 +0100

    Fix QueryHelpers.WhereIsRecentWorkingTime to not use database time

--- src/CatTime.Backend/Database/QueryHelpers.cs
+++ src/CatTime.Backend/Database/QueryHelpers.cs
@@ -6,10 +6,15 @@ public static class QueryHelpers
 {
     public static IQueryable<WorkingTime> WhereIsRecentWorkingTime(this IQueryable<WorkingTime> query)
     {
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        var now = TimeOnly.FromDateTime(DateTime.Now);
+        var yesterday = DateOnly.FromDateTime(DateTime.Today.AddDays(-1));
+        var isInMorning = DateTime.Now.Hour < 5;
+        
         // Either:
         // * the most recent time from today, or
         // * the last one from yesterday if we it's before 5am right now
-        return query.Where(f => (f.Date == DateOnly.FromDateTime(DateTime.Today) && f.Start < TimeOnly.FromDateTime(DateTime.Now)) || 
-                                (f.Date == DateOnly.FromDateTime(DateTime.Today.AddDays(-1)) && DateTime.Now.Hour < 5));
+        return query.Where(f => (f.Date == today && f.Start < now) || 
+                                (f.Date == yesterday && isInMorning));
     }
 }
